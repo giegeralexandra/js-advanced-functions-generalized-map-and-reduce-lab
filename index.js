cacd7c68264f3d26798c0259935b41266baf0265
giegeralexandra <giegeralexandra@gmail.com>
2021-02-27 19:59:08 -0500

    commit tree f275fefc839b7119130fbfae0630c491c81f51dc

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+map()
+function map(array, fn){
+    let r = []
+    for (let i = 0; i < array.length; i++ ) {
+    r.push(fn(array[i]))
+    }
+  return r
+}
+
+function reduce(array, fn, starting) {
+    let num 
+    let i=0
+    if (starting){ 
+        num = starting 
+    } else {
+        num = array[0]
+        i++
+    }
+
+    for (i; i< array.length; i++){
+        num = fn(array[i],num)
+    }
+    return num
+}
